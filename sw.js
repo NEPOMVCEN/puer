5a5d756e06a9642ca134e7f9d3a9ddc6c4494c59
NEPOMVCEN <129957166+NEPOMVCEN@users.noreply.github.com>
2026-08-31 20:35:24 +0200

    nie pamietam co to
    działamy dalej

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
    UWAGA przy aktualizacji apki: podnieś numer w CACHE poniżej (np. na 'puer-3-1').
    Zmiana nazwy powoduje pobranie plików od nowa i skasowanie starej kopii. */
-var CACHE = 'puer-3-6-8';
+var CACHE = 'puer-3-6-11';
 var PLIKI = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
